2b09606e58601c98eae6537f819f311b2b17d70f
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2023-07-15 20:28:09 -0400

    css changes and overhaul

---
+++ interactive-rating-component-main/highlight script backup.css
@@ -0,0 +1,11 @@
+var scale = document.getElementById("ratings");
+  var rating = scale.getElementsByClassName("rate");
+  for (var i = 0; i < rating.length; i++) {
+    rating[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) { 
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    this.className += " active";
+    });
+  }
