399757d3a7b2e417aaf2d8357368a8ed9ba1b884
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2023-07-22 01:32:20 -0400

    general changes

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,5 +1,13 @@
+const mainBox = document.querySelector(".box")
+const thankBox = document.querySelector(".box2")
 const rating = document.getElementById("rating")
 const rates = document.getElementById("rate")
+const submitButton = document.getElementById("submit")
+
+submitButton.addEventListener("click", () => {
+  thankBox.classList.remove("hidden")
+  mainBox.style.display = "none"
+})
 
   rates.forEach((rate) => {
     rate.addEventListener("click", () => {
