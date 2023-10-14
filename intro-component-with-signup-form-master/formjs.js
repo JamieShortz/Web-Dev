a13f83b1ffc4a315930da5d4bae3c8f4aed5f6ce
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2023-10-14 05:25:19 -0400

    Update formjs.js

--- intro-component-with-signup-form-master/formjs.js
+++ intro-component-with-signup-form-master/formjs.js
@@ -1,4 +1,30 @@
-const email = document.querySelector(".email");
+const firstName = document.getElementById("first");
+const lastName = document.getElementById("last");
+const email = document.getElementById("email");
+const pass = document.getElementById("passWord");
+const box = document.getElementById("box");
+const errorMess = document.getElementById("error");
+
+box.addEventListener('submit', (e) => {
+	let messages = []
+	if (name.value == '' || name.value == null)
+		messages.push('First Name cannot be empty')
+
+	if (messages.length > 0) {
+		e.preventDefault()
+		errorMess.innerText = messages.join(', ')
+	}
+})
+
+
+lastName.addEventListener("input", (event) => {
+	if (lastName.validity.valueMissing) {
+		lastName.setCustomValidity("Last Name cannot be empty");
+
+	} else {
+		lastName.setCustomValidity("");
+	}
+});
 
 email.addEventListener("input", (event) => {
 	if (email.validity.typeMismatch) {
@@ -6,5 +32,14 @@ email.addEventListener("input", (event) => {
 
 	} else {
 		email.setCustomValidity("");
+	}
+});
+
+pass.addEventListener("input", (event) => {
+	if (pass.validity.valueMissing) {
+		pass.setCustomValidity("Password cannot be empty");
+
+	} else {
+		pass.setCustomValidity("");
 	}
 });
