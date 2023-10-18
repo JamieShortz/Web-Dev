3e43c1563a9a9551076e6f47f46598aa80333339
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2023-10-18 00:14:37 -0400

    updates to js

--- intro-component-with-signup-form-master/formjs.js
+++ intro-component-with-signup-form-master/formjs.js
@@ -7,24 +7,19 @@ const errorMess = document.getElementById("error");
 
 box.addEventListener('submit', (e) => {
 	let messages = []
-	if (name.value == '' || name.value == null)
+	if (firstName.value == '' || firstName.value == null) {
 		messages.push('First Name cannot be empty')
+	}
+
+	if (lastName.value == '' || lastName.value == null) {
+		messages.push('Last Name cannot be empty')
+	}
 
 	if (messages.length > 0) {
 		e.preventDefault()
-		errorMess.innerText = messages.join(', ')
+		errorMess.innerText = messages.join('\n ')
 	}
 })
-
-
-lastName.addEventListener("input", (event) => {
-	if (lastName.validity.valueMissing) {
-		lastName.setCustomValidity("Last Name cannot be empty");
-
-	} else {
-		lastName.setCustomValidity("");
-	}
-});
 
 email.addEventListener("input", (event) => {
 	if (email.validity.typeMismatch) {
