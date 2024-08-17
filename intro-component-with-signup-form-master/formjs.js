49542f5f8139f2477af99ffaa1054d314bf119c1
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2024-08-16 23:10:04 -0400

    updated js

--- intro-component-with-signup-form-master/formjs.js
+++ intro-component-with-signup-form-master/formjs.js
@@ -4,15 +4,14 @@ const email = document.getElementById("email");
 const pass = document.getElementById("passWord");
 const box = document.getElementById("box");
 const errorMess = document.getElementById("error");
+const errorMess2 = document.getElementById("error2");
+const errorMess3 = document.getElementById("error3");
+const errorMess4 = document.getElementById("error4");
 
 box.addEventListener('submit', (e) => {
 	let messages = []
 	if (firstName.value == '' || firstName.value == null) {
 		messages.push('First Name cannot be empty')
-	}
-
-	if (lastName.value == '' || lastName.value == null) {
-		messages.push('Last Name cannot be empty')
 	}
 
 	if (messages.length > 0) {
@@ -21,10 +20,22 @@ box.addEventListener('submit', (e) => {
 	}
 })
 
+box.addEventListener('submit', (e) => {
+	let messages = []
+	
+	if (lastName.value == '' || lastName.value == null) {
+		messages.push('Last Name cannot be empty')
+	}
+
+	if (messages.length > 0) {
+		e.preventDefault()
+		errorMess2.innerText = messages.join('\n ')
+	}
+})
+
 email.addEventListener("input", (event) => {
 	if (email.validity.typeMismatch) {
 		email.setCustomValidity("Looks like this is not an email");
-
 	} else {
 		email.setCustomValidity("");
 	}
