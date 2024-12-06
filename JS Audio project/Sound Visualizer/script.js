c190b40a547c2623cfa25ac0b7e6698b0de9f29b
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2024-12-06 00:23:10 -0500

    Sound Visualizer update

--- JS Audio project/Sound Visualizer/script.js
+++ JS Audio project/Sound Visualizer/script.js
@@ -1,5 +1,6 @@
 const container = document.getElementById('container');
 const canvas = document.getElementById('canvas1');
+const file = document.getElementById("fileupload");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
@@ -30,7 +31,7 @@ container.addEventListener('click', function(){
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		analyser.getByteFrequencyData(dataArray);
 		for (let i = 0; i < bufferLength; i++){
-			barHeight = dataArray[i];
+			barHeight = dataArray[i] * 2;
 			ctx.fillStyle = 'white';
 			ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
 			x += barWidth;
@@ -39,3 +40,37 @@ container.addEventListener('click', function(){
 	}
 	animate();
 });
+
+file.addEventListener('change', function(){
+	const files = this.files;
+	const audio1 = document.getElementById('audio1');
+	audio1.src = URL.createObjectURL(files[0]);
+	audio1.load();
+	audio1.play();
+	audioSource = audioContext.createMediaElementSource(audio1);
+	analyser = audioContext.createAnalyser();
+	audioSource.connect(analyser);
+	analyser.connect(audioContext.destination);
+	analyser.fftSize = 64;
+	const bufferLength = analyser.frequencyBinCount;
+	const dataArray = new Uint8Array(bufferLength);
+
+	const barWidth = canvas.width/bufferLength;
+	let barHeight; //???
+	let x; //???
+
+
+	function animate(){
+		x = 0;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		analyser.getByteFrequencyData(dataArray);
+		for (let i = 0; i < bufferLength; i++){
+			barHeight = dataArray[i] * 2;
+			ctx.fillStyle = 'white';
+			ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+			x += barWidth;
+		}
+		requestAnimationFrame(animate);
+	}
+	animate();
+})
