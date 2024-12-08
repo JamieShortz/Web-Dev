89f6f0a924e0fe5c4218f46c98378f54f32590f4
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2024-12-08 01:47:27 -0500

    SV update

--- JS Audio project/Sound Visualizer/script.js
+++ JS Audio project/Sound Visualizer/script.js
@@ -17,11 +17,11 @@ container.addEventListener('click', function(){
 	analyser = audioContext.createAnalyser();
 	audioSource.connect(analyser);
 	analyser.connect(audioContext.destination);
-	analyser.fftSize = 64;
+	analyser.fftSize = 1024;
 	const bufferLength = analyser.frequencyBinCount;
 	const dataArray = new Uint8Array(bufferLength);
 
-	const barWidth = canvas.width/bufferLength;
+	const barWidth = (canvas.width/2)/bufferLength;
 	let barHeight; //???
 	let x; //???
 
@@ -42,6 +42,7 @@ file.addEventListener('change', function(){
 	audio1.src = URL.createObjectURL(files[0]);
 	audio1.load();
 	audio1.play();
+
 	audioSource = audioContext.createMediaElementSource(audio1);
 	analyser = audioContext.createAnalyser();
 	audioSource.connect(analyser);
@@ -50,7 +51,7 @@ file.addEventListener('change', function(){
 	const bufferLength = analyser.frequencyBinCount;
 	const dataArray = new Uint8Array(bufferLength);
 
-	const barWidth = canvas.width/bufferLength;
+	const barWidth = (canvas.width/2)/bufferLength;
 	let barHeight; //???
 	let x; //???
 
@@ -68,9 +69,22 @@ file.addEventListener('change', function(){
 function drawVisualizer(bufferLength, x, barWidth, barHeight, dataArray){
 	for (let i = 0; i < bufferLength; i++){
 			barHeight = dataArray[i] * 2;
-			const red = i *barHeight/20;
-			const green = i * 4;
-			const blue = barHeight/6;
+			const red = i *barHeight/30;
+			const green = i/2;
+			const blue = barHeight/2;
+			ctx.fillStyle = 'white';
+			ctx.fillRect(canvas.width/2 - x, canvas.height - barHeight - 30, barWidth, 15);
+			ctx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue +')';
+			ctx.fillRect(canvas.width/2 - x, canvas.height - barHeight, barWidth, barHeight);
+			x += barWidth;
+		}
+	for (let i = 0; i < bufferLength; i++){
+			barHeight = dataArray[i] * 2;
+			const red = i *barHeight/30;
+			const green = i/2;
+			const blue = barHeight;
+			ctx.fillStyle = 'white';
+			ctx.fillRect(x, canvas.height - barHeight - 30, barWidth, 15);
 			ctx.fillStyle = 'rgb(' + red + ',' + green + ',' + blue +')';
 			ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
 			x += barWidth;
