62a630c1f5a615448a0888f97567cfe7bba3d165
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2025-04-08 04:56:00 -0400

    updated project

--- Melody Maker app project/script.js
+++ Melody Maker app project/script.js
@@ -4,6 +4,12 @@ let CANVAS;
 function main() {
 	CANVAS = document.getElementById("myCanvas");
 	fitToScreen();
+	drawScene();
+}
+
+function drawScene(){
+	let ctx=CANVAS.getContext("2d");
+	let spacing=CANVAS.height/20;
 }
 
 function fitToScreen() {
