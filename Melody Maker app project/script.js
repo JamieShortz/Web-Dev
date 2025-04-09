1d113b74657cf8b2ad46fc1afce7b2faab8533ca
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2025-04-09 14:28:48 -0400

    Update script.js

--- Melody Maker app project/script.js
+++ Melody Maker app project/script.js
@@ -10,6 +10,15 @@ function main() {
 function drawScene(){
 	let ctx=CANVAS.getContext("2d");
 	let spacing=CANVAS.height/20;
+	ctx.strokeStyle="black";
+	ctx.lineWidth=1;
+	for(let i=-2; i<=2; i++) {
+		let y=CANVAS.height/2+i*spacing*2;
+		ctx.beginPath();
+		ctx.moveTo(0,y);
+		ctx.lineTo(CANVAS.width,y);
+		ctx.stroke();
+	}
 }
 
 function fitToScreen() {
