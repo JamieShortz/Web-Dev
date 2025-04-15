d582ef094e45a6c3a07b2a35c9681ead486ebe76
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2025-04-14 22:00:15 -0400

    Update script.js

--- Melody Maker app project/script.js
+++ Melody Maker app project/script.js
@@ -13,10 +13,37 @@ function drawNote(ctx,location) {
 	ctx.fillStyle="black";
 	ctx.strokeStyle="black";
 	ctx.lineWidth=1;
+
 	ctx.beginPath();
-	ctx.arc(location.x,location.y,SPACING,0,Math.PI*2);
+	ctx.moveTo(location.x+SPACING,
+		location.y);
+	ctx.lineTo(location.x+SPACING,
+		location.y-SPACING*5);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.lineTo(location.x+SPACING,
+		location.y-SPACING*5);
+	ctx.bezierCurveTo(
+		location.x+SPACING*2, location.y-SPACING*3,
+		location.x+SPACING*2.5, location.y-SPACING*3,
+		location.x+SPACING*2.5, location.y-SPACING*1);
+	ctx.bezierCurveTo(
+		location.x+SPACING*2.5, location.y-SPACING*2.7,
+		location.x+SPACING*2, location.y-SPACING*2.7,
+		location.x+SPACING, location.y-SPACING*4.5);
+	ctx.stroke();
+	ctx.fill();
+
+	ctx.beginPath();
+	ctx.save();
+	ctx.translate(location.x, location.y);
+	ctx.rotate(-0.2);
+	ctx.scale(1.05, 0.8);
+	ctx.arc(0,0,SPACING,0,Math.PI*2);
 	ctx.fill();
 	ctx.stroke();
+	ctx.restore();
 }
 
 function drawScene(){
