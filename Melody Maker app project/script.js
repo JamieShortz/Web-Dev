28635f6dfee5be1642d255d1994261999442ea48
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2025-04-28 05:01:15 -0400

    Update script.js

--- Melody Maker app project/script.js
+++ Melody Maker app project/script.js
@@ -11,6 +11,30 @@ let MOUSE={
 	isDown:false
 }
 
+let NOTES = ["E6", "D6", "C6", "B5", "A5", "G5", "F5", "E5", "D5", 
+	"C5", "B4", "A4", "G4", "F4", "E4", "D4", "C4", "B3", "A3", "G3", "F3"
+];
+
+let FREQ = [1318.51, 1174.66, 1046.5, 987.767, 880, 783.991, 698.456, 
+	659.255, 587.33, 523.251, 493.883, 436.04, 392.44, 349.228, 
+	329.628, 293.665, 261.626, 246.942, 220, 195.998, 174.614];
+
+let MOVING_NOTES=[];
+
+class MovingNote{
+	constructor(location){
+		let index=Math.round(location.y/SPACING);
+		this.frequency-FREQ[index];
+		this.location={
+			x:location.x,
+			y:index*SPACING
+		}
+	}
+	draw(ctx){
+		drawNote(ctx, this.location);
+	}
+}
+
 function main() {
 	CANVAS = document.getElementById("myCanvas");
 	fitToScreen();
