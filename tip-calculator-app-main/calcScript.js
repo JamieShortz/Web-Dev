f5d7f19ce2a09be17dd58f429a359256bb6de806
JamieShortz <42629465+JamieShortz@users.noreply.github.com>
2025-08-14 17:36:09 -0400

    Updated project

--- tip-calculator-app-main/calcScript.js
+++ tip-calculator-app-main/calcScript.js
@@ -1,6 +1,21 @@
-$(document).ready(function () {
-      $('button').on('click', function() {
-      $('button').removeClass('active');
-    $(this).addClass('active');
-  });
-}); 
+const bill = document.getElementById("amount");
+const numPpl = document.getElementById("number");
+const fivePer = document.getElementById("five");
+const tenPer = document.getElementById("ten");
+const fifthPer = document.getElementById("fifthteen");
+const twentyPer = document.getElementById("twentyfive");
+const fiftyPer = document.getElementById("fifty");
+const customPer = document.getElementById("Custom");
+const tipCount = document.getElementById("tipCount");
+const totalCount = document.getElementById("totalCount");
+const resetButton = document.getElementById("reset");
+
+bill.addEventListener('change', billEntry);
+numPpl.addEventListener('change', pplEntry);
+fivePer.addEventListener('click', fiveButton);
+tenPer.addEventListener('click', tenButton);
+fifthPer.addEventListener('click', fifthButton);
+twentyPer.addEventListener('click', twentyButton);
+fiftyPer.addEventListener('click', fiftyButton);
+customPer.addEventListener('change', custEntry);
+resetButton.addEventListener('click', resetCalc);
